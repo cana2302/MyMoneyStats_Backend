f235410a7c3c17d412820b686ba7d73c7adc7a0a
Franco Ariel Canavese <fcanavese@hotmail.com>
2024-12-22 11:18:15 +0100

    port-3001

--- index.js
+++ index.js
@@ -80,3 +80,9 @@ app.post('/api/bills', (request, response) => {
   bills = bills.concat(bill);
   response.json(bill);
 });
+
+// ----- PORT -------
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
